fb77155ea6abbe4e3686f21cfbf22b1eb2dd8f67
Kosuke Koshijima <kosuke.koshijima@gmail.com>
2017-04-21 11:03:38 -0500

    4/21-1

--- app/controllers/calculation_controller.rb
+++ app/controllers/calculation_controller.rb
@@ -7,4 +7,15 @@ def flex_square
 
 render("calculation/calculation.html.erb")
 end
+
+def flex_square_root
+  @user_provided_number = params["the_number"].to_f
+  @squared_root_number = Math.sqrt(@user_provided_number)
+
+render("calculation/flex_square_root.html.erb")
+
+end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   get("/flexible/square/:the_number", { :controller => "calculation", :action => "flex_square" })
 
+  get("/flexible/square_root/:the_number", { :controller => "calculation", :action => "flex_square_root" })
+
 end
