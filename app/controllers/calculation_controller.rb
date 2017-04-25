8dcba8bc2d5bd4d93384fde2d217cb1cf97ec1fd
Kosuke Koshijima <kosuke.koshijima@gmail.com>
2017-04-24 21:56:18 -0500

    4/24

--- app/controllers/calculation_controller.rb
+++ app/controllers/calculation_controller.rb
@@ -10,12 +10,67 @@ end
 
 def flex_square_root
   @user_provided_number = params["the_number"].to_f
-  @squared_root_number = Math.sqrt(@user_provided_number)
+  @squared_root_number = Math.sqrt(@user_provided_number).round(2)
 
 render("calculation/flex_square_root.html.erb")
 
 end
 
+def payment_calculation
+  @user_provided_bp = params["basis_points"].to_f
+  @user_provided_years = params["number_of_years"].to_f
+  @user_provided_pv = params["present_value"].to_f
+
+@monthly_payment = ((@user_provided_bp / 12 / 100 / 100 * @user_provided_pv) / (1 - ((1 + @user_provided_bp / 12 / 100 / 100) ** ((-1) * @user_provided_years * 12)))).round(2)
+
+render("calculation/payment_calculation.html.erb")
+end
+
+def random
+  @user_provided_min = params["min"].to_f
+  @user_provided_max = params["max"].to_f
+  @randome_number = rand(@user_provided_min..@user_provided_max)
+
+render("calculation/random_number.html.erb")
+end
+
+
+
+def square_form
+render("calculation/square_form.html.erb")
+end
+
+def square
+  @user_provided_number = params["number_to_be_squared"].to_f
+  @squared_number = @user_provided_number ** 2
+render("calculation/square.html.erb")
+end
+
+def square_root_form
+render("calculation/square_root_form.html.erb")
+end
+
+def square_root
+  @user_provided_number = params["number_to_be_rooted"].to_f
+  @squared_root_number = Math.sqrt(@user_provided_number).round(2)
+
+render("calculation/square_root.html.erb")
+end
+
+def payment_form
+render("calculation/payment_form.html.erb")
+end
+
+def payment
+  @user_provided_bp = params["user_apr"].to_f
+  @user_provided_years = params["user_years"].to_f
+  @user_provided_pv = params["user_pv"].to_f
+
+@monthly_payment = ((@user_provided_bp / 12 / 100 / 100 * @user_provided_pv) / (1 - ((1 + @user_provided_bp / 12 / 100 / 100) ** ((-1) * @user_provided_years * 12)))).round(2)
+
+render("calculation/payment.html.erb")
+
+end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get("/flexible/square/:the_number", { :controller => "calculation", :action => "flex_square" })
-
   get("/flexible/square_root/:the_number", { :controller => "calculation", :action => "flex_square_root" })
+  get("/flexible/payment/:basis_points/:number_of_years/:present_value", { :controller => "calculation", :action => "payment_calculation" })
+  get("/flexible/random/:min/:max", { :controller => "calculation", :action => "random" })
+
+  get("/square/new", { :controller => "calculation", :action => "square_form" })
+  get("/square/results", { :controller => "calculation", :action => "square" })
+
+  get("/square_root/new", { :controller => "calculation", :action => "square_root_form" })
+  get("/square_root/results", { :controller => "calculation", :action => "square_root" })
+
+  get("/payment/new", { :controller => "calculation", :action => "payment_form" })
+  get("/payment/results", { :controller => "calculation", :action => "payment" })
+
+  get("/random/new", { :controller => "calculation", :action => "payment_form" })
+
+
+
 
 end
